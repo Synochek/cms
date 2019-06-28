32b19ac07196fa0c1438eab2f736e0418637b8fd
Synochek <pteeradukteel@gmail.com>
2019-06-28 21:48:23 +0300

    3 урок. Перед последним повторением

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from django.views import View
 from .models import Post
 
-class PostList(View):
+
+class PostList(View):  # as_View запускает нужный метод класса с таким же именем, как имя метода
     def get(self, request):
-        posts = Post.object
+        posts = Post.objects.all()
+        print(posts)
+        return render(request, 'blog/postlist.html', {'post_list': posts})
